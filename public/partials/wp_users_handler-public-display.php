ee87172bd49baf0452c2417f8f05fc1a87fdd02b
Mustafa Shaaban <mustafashaaban22@gmail.com>
2019-10-16 16:22:26 +0200

    init

---
+++ public/partials/wp_users_handler-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://www.linkedin.com/in/mustafa-shaaban22/
+ * @since      1.0.0
+ *
+ * @package    Wp_users_handler
+ * @subpackage Wp_users_handler/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
